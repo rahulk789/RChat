c1b44c3feab2fa461f7fbb316ff86f722bf64bef
rahulk789 <rahul.u.india@gmail.com>
2023-01-12 11:59:14 +0000

    removed heroku

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 
-const socket = io('http://rchat7.herokuapp.com:8000');
+const socket = io('http://localhost:8000');
 const form =document.getElementById('send-container');
 const messageInput =document.getElementById('messageInp');
 const messageContainer =document.querySelector(".container");
