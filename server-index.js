c68c1cbe88a15dc0dc04a64af554ff4240e17292
rahulk789 <rahul.u.india@gmail.com>
2023-06-25 14:23:50 +0530

    helm waf

--- server-index.js
+++ server-index.js
@@ -1,4 +1,3 @@
-// index.js
 
 const express = require("express")
 const helmet = require("helmet")
@@ -18,21 +17,4 @@ app.get("/", (req, res) => {
 // running the server
 app.listen(PORT, () => {
     console.log(`Starting Express server on http://localhost:${PORT}`)
-})/*
-const express = require("express");
-const helmet = require("helmet")
-
-const app = express();
-app.use(express.static(__dirname));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-const PORT = process.env.PORT || 4099
-
-
-// Creating https server by passing
-app.use(helmet());
-app.listen(PORT, function (req, res) {
-  console.log("Server started at port 4099");
-});
-*/
+})
