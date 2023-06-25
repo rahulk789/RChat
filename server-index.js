d7dd616c0df0361cc1698eed7ca961d557b6034d
rahulk789 <rahul.u.india@gmail.com>
2023-06-25 14:22:25 +0530

    helm waf

--- server-index.js
+++ server-index.js
@@ -1,35 +1,38 @@
-const express = require("express");
-const https = require("https");
-const http = require("http");
-const fs = require("fs");
-var express_waf_middleware = require('express-waf-middleware');
+// index.js
 
-const app = express();
-// serve your css as static
+const express = require("express")
+const helmet = require("helmet")
+
+const PORT = process.env.PORT || 4099
+
+const app = express()
+
+// enabling the Helmet middleware
+app.use(helmet())
+
 app.use(express.static(__dirname));
-
-var waf_options = {
-    url: 1, //detect the url,1:enable,0:disable,below,the same
-    id: 1,
-    userAgent: 1,   //detect the user-agent
-    cookies: 1, //detect the cookies
-    body: 1,    //detect the body
-    log: 1  //print log informaton
-}
-app.use(express_waf_middleware(waf_options));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// for SSL
-const options = {
-  key: fs.readFileSync("./certs/server.key"),
-  cert: fs.readFileSync("./certs/server.pem"),
-};
-  
+// running the server
+app.listen(PORT, () => {
+    console.log(`Starting Express server on http://localhost:${PORT}`)
+})/*
+const express = require("express");
+const helmet = require("helmet")
+
+const app = express();
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+const PORT = process.env.PORT || 4099
+
+
 // Creating https server by passing
-// options and app object
-http.createServer(app)
-.listen(4099, function (req, res) {
+app.use(helmet());
+app.listen(PORT, function (req, res) {
   console.log("Server started at port 4099");
 });
+*/
